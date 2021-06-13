50538f3d88a2fe1a6bd5a5b86942aa26def033cd
Michael Abbott <michael@mjabbottdesign.com>
2021-06-13 14:45:07 -0700

    Add readable attributes test; update defaul attributes test

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -17,8 +17,19 @@ RSpec.describe Shifts do
     end
 
     it 'has readable default attributes' do
-      expect(@shifts.keys).to eq('random')
-      expect(@shifts.date).to eq('todays date')
+      expect(@shifts.keys).to be_an_instance_of(Keys)
+      expect(@shifts.offsets).to be_an_instance_of(Offsets)
+      expect(@shifts.keys.key).to eq('random')
+      expect(@shifts.offsets.date).to eq('todays date')
+    end
+
+    it 'has readable attributes' do
+      @shifts2 = Shifts.new('02715', '040895')
+
+      expect(@shifts2.keys).to be_an_instance_of(Keys)
+      expect(@shifts2.offsets).to be_an_instance_of(Offsets)
+      expect(@shifts2.keys.key).to eq('02715')
+      expect(@shifts2.offsets.date).to eq('040895')
     end
   end
 end
