5424439cd37e2a44357f325522b9e7f9aae94662
Michael Abbott <michael@mjabbottdesign.com>
2021-06-13 15:47:28 -0700

    Add @shift_hash call to #shifts_hash_builder; :shifts_hash attr_reader

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,14 +1,15 @@
 class Shifts
-  attr_reader :keys, :offsets
+  attr_reader :keys, :offsets, :shifts_hash
 
   def initialize(keys_input = 'random', date_input = 'todays date')
     @keys = Keys.new(keys_input)
     @offsets = Offsets.new(date_input)
+    @shifts_hash = shifts_hash_builder
   end
 
   def shifts_hash_builder
-    @keys.keys_hash.merge!(@offsets.offsets_hash) do |letter, key_val, offset_val|
-      key_val + offset_val
+    @keys.keys_hash.merge!(@offsets.offsets_hash) do |letter, key, offset|
+      key + offset
     end
   end
 end
