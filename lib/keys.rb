1f921166da4917df96e3d3e33976c9ff42535c70
Michael Abbott <michael@mjabbottdesign.com>
2021-06-12 17:08:11 -0700

    Add Keys #random_key

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,6 +8,11 @@ class Keys
   def verify_key(input)
     return input if input == 'random'
     input.is_a?(Integer) && input.to_s.length == 5 ?
-      input : 'invalid'
+      input.to_s : 'invalid'
+  end
+
+  def random_key
+    string_key = rand(99_999).to_s
+    format('%05d', string_key)
   end
 end
