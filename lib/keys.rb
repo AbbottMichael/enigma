59af5a0d5a93cd5c1f1338201b788aa7ea80f920
Michael Abbott <michael@mjabbottdesign.com>
2021-06-12 21:54:49 -0700

    Add new attributes and #keys_hash_builder

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,8 +1,10 @@
 class Keys
-  attr_reader :key
+  attr_reader :key, :verified_key, :keys_hash
 
   def initialize(key)
     @key = key
+    @verified_key = verify_key(@key)
+    @keys_hash = {}
   end
 
   def verify_key(input)
@@ -18,5 +20,14 @@ class Keys
   def random_key
     string_key = rand(99_999).to_s
     format('%05d', string_key)
+  end
+
+  def keys_hash_builder
+    @keys_hash.merge!(
+      A: (@verified_key[0] + @verified_key[1]).to_i,
+      B: (@verified_key[1] + @verified_key[2]).to_i,
+      C: (@verified_key[2] + @verified_key[3]).to_i,
+      D: (@verified_key[3] + @verified_key[4]).to_i
+    )
   end
 end
