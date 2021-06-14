e28d63c9af3fb8c0a2da5b76f1ea1c2e496729ba
Michael Abbott <michael@mjabbottdesign.com>
2021-06-13 21:07:40 -0700

    Add #decrypt and #decryption

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,7 +8,7 @@ class Enigma
 
   def encrypt(message, key = 'random', date = 'date today')
     shifts = Shifts.new(key, date)
-    final = {
+    {
       :encryption => encryption(message, shifts),
       :key => shifts.keys.verified_key,
       :date => shifts.offsets.verified_date
@@ -25,6 +25,29 @@ class Enigma
       shift_position = @shift_count.rotate(counter)
       shift = shifts.shifts_hash.values[shift_position[0]]
       shifted_char_set = @character_set.rotate(shift)
+      shifted_char_set[index_value]
+    end.join
+  end
+
+  def decrypt(ciphertext, key, date = 'date today')
+    shifts = Shifts.new(key, date)
+    {
+      :decryption => decryption(ciphertext, shifts),
+      :key => shifts.keys.verified_key,
+      :date => shifts.offsets.verified_date
+    }
+  end
+
+  def decryption(ciphertext, shifts)
+    ciphertext_array = ciphertext.downcase.split('')
+    counter = -1
+    ciphertext_array.map do |letter|
+      counter += 1
+      index_value = @character_set.find_index(ciphertext_array[counter])
+      next ciphertext_array[counter] if index_value == nil
+      shift_position = @shift_count.rotate(counter)
+      shift = shifts.shifts_hash.values[shift_position[0]]
+      shifted_char_set = @character_set.rotate(-shift)
       shifted_char_set[index_value]
     end.join
   end
