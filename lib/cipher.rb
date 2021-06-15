f2ace5cbe59c4ae9f13e25543d6f9488a228d66e
Michael Abbott <michael@mjabbottdesign.com>
2021-06-14 20:30:41 -0700

    Add Cipher #initialize and attr_reader

---
+++ lib/cipher.rb
@@ -0,0 +1,10 @@
+class Cipher
+  attr_reader :output_message
+
+  def initialize(message, shifts, type)
+    @input_message = message
+    @output_message = 'text'
+    @shifts = shifts
+    @type = type
+  end
+end
