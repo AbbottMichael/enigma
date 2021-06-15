11f08fb3c3ec32c6909acf355bb5a98942a2205a
Michael Abbott <michael@mjabbottdesign.com>
2021-06-14 20:34:34 -0700

    Add #character_set test

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -21,4 +21,17 @@ RSpec.describe Cipher do
       expect(@cipher.output_message).to eq('text')
     end
   end
+
+  describe 'methods' do
+
+    it 'can generate the character_set' do
+      expected = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z', ' '
+      ]
+
+      expect(@cipher.character_set).to eq(expected)
+    end
+  end
 end
